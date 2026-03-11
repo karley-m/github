727efdd4e2879d9b4858d89f710792cc20b24897
karley-m <kbmakar@ucdavis.edu>
2026-03-11 14:59:58 -0700

    splitting the deck

--- game/v2-final-version/script.js
+++ game/v2-final-version/script.js
@@ -29,8 +29,6 @@
     const score1 = document.querySelector('#score1');
     const score2 = document.querySelector('#score2');
 
-    
-    
     const gameData = {
         cards: [
                 {img:'2black.png', value:2}, {img:'2red.png',value:2},
@@ -50,6 +48,8 @@
         players: ['player 1', 'player 2'],
         avatars: ['avatar1.png', 'avatar2.png', 'avatar3.png', 'avatar4.png'],
         score: [0,0],
+        player1Deck: [],
+        player2Deck: [],
         round: 0,
         draw1: 0,
         draw2: 0,
@@ -57,6 +57,9 @@
         index: 0,
         gameEnd:190 
     };
+
+    const card1 = gameData.player1Deck.shift();
+    const card2 = gameData.player2Deck.shift();
     
     // ------------- switching from start screen to avatar selection screen --------------
     document.querySelector('#gotoavatar').addEventListener ('click', function(event){
@@ -142,6 +145,7 @@
 
         setTimeout(function(){
             deck.classList.remove('shuffling');
+            setupDecks();
             dealCards();
             shuffleButton.style.display = 'none';
             setTimeout(function(){
@@ -150,6 +154,24 @@
         }, 800);
 
     });
+
+    function shuffleDeck(deck){
+        for(let i = deck.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [deck[i], deck[j]] = [deck[j], deck[i]];
+        }
+        return deck;
+    }
+
+    function setupDecks(){
+        const shuffled = shuffleDeck([...gameData.cards]);
+
+        gameData.player1Deck = shuffled.slice(0,13);
+        gameData.player2Deck = shuffled.slice(13,26);
+
+        console.log('p1 deck:', gameData.player1Deck);
+        console.log('p2 deck:', gameData.player2Deck);
+    }
 
     // ------------- sending cards to each side of screen for players --------------
     function dealCards(){
@@ -166,6 +188,10 @@
             cards[2].style.transform = "translate(-420px, 230px)";
         }, 400);
 
+        setTimeout(function(){
+            cards[6].style.transform = "translate(-420px, 230px)";
+        }, 600);
+
         // Player 2 (right side)
         setTimeout(function(){
             cards[3].style.transform = "translate(420px, 230px)";
@@ -178,6 +204,11 @@
         setTimeout(function(){
             cards[5].style.transform = "translate(420px, 230px)";
         }, 400);
+
+        setTimeout(function(){
+            cards[7].style.transform = "translate(420px, 230px)";
+        }, 600);
+        
     };
 
 
@@ -213,55 +244,90 @@
     function flipCards(){
         roundCount.innerHTML = `Round ${gameData.round}`;
         
+        const card1 = gameData.player1Deck.shift();
+        const card2 = gameData.player2Deck.shift();
         
-        gameData.draw1 = Math.floor(Math.random() * gameData.cards.length);
-        gameData.draw2 = Math.floor(Math.random() * gameData.cards.length);
+        // gameData.draw1 = Math.floor(Math.random() * gameData.cards.length);
+        // gameData.draw2 = Math.floor(Math.random() * gameData.cards.length);
 
-        front1.innerHTML = `<img src="images/${gameData.cards[gameData.draw1].img}">`;
-        front2.innerHTML = `<img src="images/${gameData.cards[gameData.draw2].img}">`;
+        front1.innerHTML = `<img src="images/${card1.img}">`;
+        front2.innerHTML = `<img src="images/${card2.img}">`;
+        // front1.innerHTML = `<img src="images/${gameData.cards[gameData.draw1].img}">`;
+        // front2.innerHTML = `<img src="images/${gameData.cards[gameData.draw2].img}">`;
 
         front1.classList.add('flipFront');
         front2.classList.add('flipFront');
 
-        // gameData.drawSum = gameData.draw1 + gameData.draw2;
-        gameData.drawSum = gameData.cards[gameData.draw1].value + gameData.cards[gameData.draw2].value;
+        gameData.drawSum = card1.value + card2.value;
+        // gameData.drawSum = gameData.cards[gameData.draw1].value + gameData.cards[gameData.draw2].value;
 
-        console.log(gameData.drawSum);
-        console.log('p1', gameData.cards[gameData.draw1].value);
-        console.log('p2', gameData.cards[gameData.draw2].value);
+
+        console.log('drawsum:',gameData.drawSum);
+        console.log('p1 card value:', card1.value);
+        console.log('p2 card value:', card2.value);
+
+        // console.log("After draw:");
+        // console.log("P1:", gameData.player1Deck.length);
+        // console.log("P2:", gameData.player2Deck.length);
 
         setTimeout(function(){
             // if player 1's card is higher
-            if ((gameData.cards[gameData.draw1].value) > (gameData.cards[gameData.draw2].value)) {
+            if (card1.value > card2.value) {
                 console.log('player 1 won this round with points!');
                 setTimeout(function(){
                     moveCards1();
-                }, 1000);
+                }, 1500);
 
                 gameData.score[0] += gameData.drawSum;
                 updateRoundWin.innerHTML = 'Player 1 drew the higher card!';
                 score1.innerHTML = `Score: ${gameData.score[0]}`;
+
+                gameData.player1Deck.push(card1);
+                gameData.player1Deck.push(card2);
+
+                checkGameEnd();
             }
 
             // if player 2's card is higher
-            if ((gameData.cards[gameData.draw1].value) < (gameData.cards[gameData.draw2].value)) {
+            if (card1.value < card2.value) {
                 console.log('player 2 won this round with points!');
                 setTimeout(function(){
                     moveCards2();
-                }, 1000);
+                }, 1500);
 
                 gameData.score[1] += gameData.drawSum;
                 updateRoundWin.innerHTML = 'Player 2 drew the higher card!';
                 score2.innerHTML = `Score: ${gameData.score[1]}`;
+
+                gameData.player2Deck.push(card1);
+                gameData.player2Deck.push(card2);
+
+                checkGameEnd();
             }
 
             // if the player's cards are equal
-            if ((gameData.cards[gameData.draw1].value) == (gameData.cards[gameData.draw2].value)) {
+            if (card1.value === card2.value) {
                 console.log('war!!!!');
                 updateRoundWin.innerHTML = "War! Click 'Flip Cards' again!";
             }
+
+            console.log("After round:");
+            console.log("P1:", gameData.player1Deck.length);
+            console.log("P2:", gameData.player2Deck.length);
         }, 1000);
         
+    }
+
+    function checkGameEnd(){
+        if(gameData.player1Deck.length === 0){
+            updateRoundWin.innerHTML = "Player 2 wins the game!";
+            document.querySelector('#flip').style.display = 'none';
+        };
+
+        if(gameData.player2Deck.length === 0){
+            updateRoundWin.innerHTML = "Player 1 wins the game!";
+            document.querySelector('#flip').style.display = 'none';
+        }
     }
 
     function moveCards1(){
@@ -275,6 +341,9 @@
         front1.classList.add('collect2');
         front2.classList.add('collect2');
     }
+
+    console.log("P1 deck:", gameData.player1Deck.length);
+    console.log("P2 deck:", gameData.player2Deck.length);
     
 
 
